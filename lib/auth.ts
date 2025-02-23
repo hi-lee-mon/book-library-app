125ad564a40af08b74635f223f1e9ecf5619ea24
hi-lee-mon <shunmel.09@gmail.com>
2025-02-24 00:57:06 +0900

    🔒 chore(auth): Add AUTH_SECRET to NextAuth configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,7 @@ import { PrismaAdapter } from '@auth/prisma-adapter'
 import NextAuth from 'next-auth'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   session: {
     strategy: 'jwt',
